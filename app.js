e8f365fc399031a048e2771a69f7024e7e05e5a3
your name <nghiatran00932@gmail.com>
2023-09-12 22:00:57 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 
 const contactsRouter = require('./app/routes/contact.route');
 
+const ApiError = require('./app/api-error');
+
 const app = express();
 
 app.use(cors());
@@ -10,4 +12,15 @@ app.use(express.json());
 
 app.use('/api/contacts', contactsRouter);
 
+// handle 404 respose
+app.use((req, res, next) => {
+  return next(new ApiError(404, 'Resource not found'));
+});
+
+//defiine error-handing middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || 'Internal Server Error',
+  });
+});
 module.exports = app;
