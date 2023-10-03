3584cc9b8cac7709cf8f09e29f612487c0494e41
your name <nghiatran00932@gmail.com>
2023-10-03 14:35:13 +0700

    Cai dat cac handler truy xuat CSDL

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -12,9 +12,9 @@ router
 router.route('/favorite').get(contacts.findAllFavorite);
 
 router
-  .route(':id')
+  .route('/:id')
   .get(contacts.findOne)
-  .put(contacts.delete)
+  .put(contacts.update)
   .delete(contacts.delete);
 
 module.exports = router;
